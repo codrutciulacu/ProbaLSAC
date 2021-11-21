84f7662de4092a02fad4812e4db884ee05f86734
codrutciulacu <codrut.c.ciulacu@gmail.com>
2021-11-21 19:59:40 +0200

    Initial commit

---
+++ backend/src/main/java/com/codrut/probalsac/ProbalsacApplication.java
@@ -0,0 +1,13 @@
+package com.codrut.probalsac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbalsacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbalsacApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/codrut/probalsac/ProbalsacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codrut.probalsac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbalsacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

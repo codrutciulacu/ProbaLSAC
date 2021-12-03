22367ad25e88cb98fb0d2886b9825070abaf77d8
codrut.ciulacu <codrut.c.ciulacu@gmail.com>
2021-12-03 21:50:04 +0200

    some changes

--- backend/src/main/java/com/codrut/probalsac/review/validation/ReviewValidation.java
+++ backend/src/main/java/com/codrut/probalsac/review/validation/ReviewValidation.java
@@ -9,8 +9,7 @@ public class ReviewValidation {
     public void validate(ReviewCreationDTO dto) {
         if(!(dto.message.length() <= 500 && !dto.message.isEmpty()))
             throw new RuntimeException("The message is too short or to long!");
-        if(dto.user_id < 0)
-            throw new RuntimeException("The message is too short or to long!");
+
     }
 
 }
